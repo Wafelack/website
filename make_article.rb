2b3677a35219b520e19cc32046b222a0d8bf6bee
Wafelack <wafelack@wafelack.fr>
2021-07-17 21:59:34 +0200

    blog: Change blog organisation.

--- make_article.rb
+++ make_article.rb
@@ -3,9 +3,7 @@ require 'org-ruby'
 require 'fileutils'
 
 def make_article_path(title)
-  dirname = Time.now.strftime("articles/%Y/%m/%d/")
-  FileUtils.mkdir_p dirname
-  "#{dirname}/#{title.gsub(' ', '_').downcase}"
+  "articles/#{title.gsub(' ', '_').downcase}"
 end
 
 def compile_article(title)
@@ -21,12 +19,12 @@ def compile_article(title)
       <meta name=keywords content=\"Programming, Wafelack, Rust, Free Software\" />
       <meta name=\"description\" content=\"Wafelack's blog\" />
       <title>#{title}</title>
-      <link rel=icon href=../../../../assets/icon.png />
-      <link rel=stylesheet type=text/css href=../../../../css/master.css />
+      <link rel=icon href=../assets/icon.png />
+      <link rel=stylesheet type=text/css href=../master.css />
   </head>
   <body>
     #{body}
-    <a href=\"../../../../\">Go To Main Page</a>
+    <a href=\"../\">Go To Main Page</a>
     </body>
     <footer><hr>Copyright (C) 2021 Wafelack<br>All this website's content is licensed under <a href=\"https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1\">CC BY-SA 4.0</a> license.</footer>
 </html>"}
@@ -44,8 +42,8 @@ if ARGV[0] == "compile"
   compile_article aname 
 elsif ARGV[0] == "new"
   fname = make_article_path(aname)
-  File.open("#{fname}.org", "w") { |f| f.write "* #{aname}\n" }
-  puts "Source file is located at #{fname}.org (output: #{fname}.html)."
+  File.open("#{fname}.org", "w") { |f| f.write "* #{aname}\n/#{Time.now.strftime("%Y-%m-%d")}/\n" }
+  puts "Source file is located at `#{fname}.org.`"
 else
   STDERR.puts "Invalid action: #{ARGV[0]}.\nAvailable actions: compile,new."
 end
